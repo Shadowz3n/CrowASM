3a770ab9c91341a64ca255d67f14c245d9c52f6f
Henrique Bissoli Silva <emp.shad@gmail.com>
2018-01-07 03:14:57 -0200

    Add files via upload

---
+++ args.asm
@@ -0,0 +1,18 @@
+%include        'crow_funcs.asm'
+ 
+SECTION .text
+global  _start
+
+_start:
+    pop     ecx
+ 
+nextArg:
+    cmp     ecx, 0h
+    jz      noMoreArgs
+    pop     eax
+    call    sprintLF
+    dec     ecx
+    jmp     nextArg
+ 
+noMoreArgs:
+    call    quit
